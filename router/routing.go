0cc40ee221ed9d9e5fcd4c533c0bcbb0b6b0948d
unknown <181220033@nitdelhi.ac.in>
2023-01-26 22:29:11 +0530

    updated version

--- router/routing.go
+++ router/routing.go
@@ -10,8 +10,8 @@ import (
 func HandlerRouting(c Controller) {
 	r := mux.NewRouter()
 	r.HandleFunc("/studentpost", c.CreateStudent).Methods("POST") //HandleFunc registers a new route with a matcher for the URL path.
-	r.HandleFunc("/studentget", c.GetStudent).Methods("GET")
-	r.HandleFunc("/studentget/{eid}", c.GetStudentByID).Methods("GET")
+	r.HandleFunc("/studentget", c.GetStudent).Methods("POST")
+	r.HandleFunc("/studentget/{eid}", c.GetStudentByID).Methods("POST")
 	r.HandleFunc("/studentupdate/{eid}", c.UpdateStudent).Methods("POST")
 	r.HandleFunc("/studentdelete/{id}", c.DeleteStudent).Methods("POST")
 
